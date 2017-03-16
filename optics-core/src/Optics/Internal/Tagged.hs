6cb7a0f6949c1096f2ee54b4db439caaf8f58758
Andres Loeh <andres@well-typed.com>
2017-03-16 10:32:35 +0100

    Add operations on reviews.

---
+++ optics-core/src/Optics/Internal/Tagged.hs
@@ -0,0 +1,30 @@
+-- | Taken from the tagged package.
+--
+-- We include this here, at least for now, with the goal
+-- that we only depend on base.
+--
+module Optics.Internal.Tagged where
+
+import Data.Bifunctor
+
+import Optics.Internal.Profunctor
+
+newtype Tagged a b = Tagged { unTagged :: b }
+
+instance Functor (Tagged a) where
+  fmap f = Tagged . f . unTagged
+  {-# INLINE fmap #-}
+
+instance Bifunctor Tagged where
+  bimap _f g = Tagged . g . unTagged
+  {-# INLINE bimap #-}
+
+instance Profunctor Tagged where
+  dimap _f g = Tagged . g . unTagged
+  {-# INLINE dimap #-}
+
+instance Choice Tagged where
+  left'  = Tagged . Left  . unTagged
+  {-# INLINE left' #-}
+  right' = Tagged . Right . unTagged
+  {-# INLINE right' #-}
