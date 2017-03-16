a5107ad237ec42b45a0b814f813ae5508bb288fc
Andres Loeh <andres@well-typed.com>
2017-03-16 10:34:36 +0100

    Enable -Wall.

--- optics-core/src/Optics.hs
+++ optics-core/src/Optics.hs
@@ -7,7 +7,7 @@ import Optics.Fold      as O
 import Optics.Getter    as O
 import Optics.Iso       as O
 import Optics.Lens      as O
-import Optics.Optic     as O
+import Optics.Optic     as O ()
 import Optics.Prism     as O
 import Optics.Review    as O
 import Optics.Setter    as O
