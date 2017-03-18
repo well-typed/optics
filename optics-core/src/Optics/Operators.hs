ac1a6adbce83b1f8b9ecf1ae6afb66650e2e9899
Andres Loeh <andres@well-typed.com>
2017-03-18 15:54:48 +0100

    Add a few frequently used infix operators.
    Same names are used as in lens.
    The operators have to be imported explicitly.

---
+++ optics-core/src/Optics/Operators.hs
@@ -0,0 +1,55 @@
+{-# LANGUAGE FlexibleContexts #-}
+
+-- | Defines some infix operators for optics operations.
+--
+-- These are not exported by default from 'Optics'.
+-- They have to be imported separately.
+--
+module Optics.Operators
+  ( (&)
+  , (^.)
+  , (^..)
+  , (^?)
+  , (%~)
+  , (.~)
+  )
+  where
+
+import Data.Function
+
+import Optics
+
+-- | Flipped infix version of 'view'.
+(^.) :: Is k A_Getter => s -> Optic' k s a -> a
+(^.) = flip view
+{-# INLINE (^.) #-}
+
+infixl 8 ^.
+
+-- | Flipped infix version of 'toListOf'.
+(^..) :: Is k A_Fold => s -> Optic' k s a -> [a]
+(^..) = flip toListOf
+{-# INLINE (^..) #-}
+
+infixl 8 ^..
+
+-- | Flipped infix version of 'preview'.
+(^?) :: Is k A_Fold => s -> Optic' k s a -> Maybe a
+(^?) = flip preview
+{-# INLINE (^?) #-}
+
+infixl 8 ^?
+
+-- | Infix version of 'over'.
+(%~) :: Is k A_Setter => Optic k s t a b -> (a -> b) -> s -> t
+(%~) = over
+{-# INLINE (%~) #-}
+
+infixr 4 %~
+
+-- | Infix version of 'set'.
+(.~) :: Is k A_Setter => Optic k s t a b -> b -> s -> t
+(.~) = set
+{-# INLINE (.~) #-}
+
+infixr 4 .~
