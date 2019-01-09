ae1b3e0fb92391fd9cb248784e7d65d4d94baea3
Oleg Grenrus <oleg@well-typed.com>
2019-01-09 14:15:19 +0200

    Document lens

--- optics-core/src/Optics/Lens.hs
+++ optics-core/src/Optics/Lens.hs
@@ -1,11 +1,50 @@
+-- | 'Lens' is a generalised or first-class *field*.
+--
+-- If we have a value @s :: S@, and a @l :: 'Lens'' S A@,
+-- we can /get/ the "field value" of type @A@ using 'view'.
+-- We can also /update/ (or /put/ or /set/) the value using 'over' (or 'set').
+--
+-- >>> data Human = Human { _name :: String, _location :: String } deriving Show
+-- >>> let human = Human "Bob" "London"
+--
+-- We can make a 'Lens' for @_name@ field:
+--
+-- >>> let name = lens _name $ \s x -> s { _name = x }
+--
+-- Which we can use as a 'Getter'
+--
+-- >>> view name human
+-- "Bob"
+--
+-- or a 'Setter'
+--
+-- >>> set name "Robert" human
+-- Human {_name = "Robert", _location = "London"}
+--
 module Optics.Lens
-  ( A_Lens
+  (
+  -- * Formation
+    A_Lens
   , Lens
   , Lens'
+
+  -- * Introduction
+  , lens
+  , toLens
+
+  -- * Elimination
+  -- | 'Lens' is a 'Setter' and a 'Getter', therefore you can
+  --
+  -- @
+  -- 'view1' :: 'Lens' i s t a b -> s -> a
+  -- 'set'   :: 'Lens' i s t a b -> b -> s -> t
+  -- 'over'  :: 'Lens' i s t a b -> (a -> b) -> s -> t
+  -- @
+  --
+
+  -- * van Laarhoven encoding
   , LensVL
   , LensVL'
-  , toLens
-  , lens
   , lensVL
   , toLensVL
   , withLensVL
@@ -16,3 +55,6 @@ module Optics.Lens
 import Optics.Internal.Lens
 import Optics.Internal.Optic
 import Optics.Optic
+
+-- $setup
+-- >>> import Optics
