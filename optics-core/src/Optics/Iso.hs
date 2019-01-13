d54658e52371aa6d31a0bc047a856f2e9b8aceb1
Andrzej Rybczak <andrzej@rybczak.net>
2019-01-13 11:13:51 +0100

    Add a few util functions to Optics.Iso

--- optics-core/src/Optics/Iso.hs
+++ optics-core/src/Optics/Iso.hs
@@ -5,10 +5,88 @@ module Optics.Iso
   , toIso
   , iso
   , withIso
+  -- * Isomorphisms
+  , mapping
+  , curried
+  , uncurried
+  , flipped
+  , Swapped(..)
   , module Optics.Optic
   )
   where
 
+import Data.Tuple
+import Data.Bifunctor
+
 import Optics.Internal.Iso
 import Optics.Internal.Optic
 import Optics.Optic
+
+-- | This can be used to lift any 'Iso' into an arbitrary 'Functor'.
+mapping
+  :: (Is k An_Iso, Functor f, Functor g)
+  => Optic k i i s t a b
+  -> Iso i (f s) (g t) (f a) (g b)
+mapping k = withIso k $ \sa bt -> iso (fmap sa) (fmap bt)
+{-# INLINE mapping #-}
+
+-- | The canonical isomorphism for currying and uncurrying a function.
+--
+-- @
+-- 'curried' = 'iso' 'curry' 'uncurry'
+-- @
+--
+-- >>> (fst^.curried) 3 4
+-- 3
+--
+-- >>> view curried fst 3 4
+-- 3
+curried :: Iso i ((a, b) -> c) ((d, e) -> f) (a -> b -> c) (d -> e -> f)
+curried = iso curry uncurry
+{-# INLINE curried #-}
+
+-- | The canonical isomorphism for uncurrying and currying a function.
+--
+-- @
+-- 'uncurried' = 'iso' 'uncurry' 'curry'
+-- @
+--
+-- @
+-- 'uncurried' = 'from' 'curried'
+-- @
+--
+-- >>> ((+)^.uncurried) (1,2)
+-- 3
+uncurried :: Iso i (a -> b -> c) (d -> e -> f) ((a, b) -> c) ((d, e) -> f)
+uncurried = iso uncurry curry
+{-# INLINE uncurried #-}
+
+-- | The isomorphism for flipping a function.
+--
+-- >>> ((,)^.flipped) 1 2
+-- (2,1)
+flipped :: Iso i (a -> b -> c) (a' -> b' -> c') (b -> a -> c) (b' -> a' -> c')
+flipped = iso flip flip
+{-# INLINE flipped #-}
+
+-- | This class provides for symmetric bifunctors.
+class Bifunctor p => Swapped p where
+  -- |
+  -- @
+  -- 'swapped' '.' 'swapped' ≡ 'id'
+  -- 'first' f '.' 'swapped' = 'swapped' '.' 'second' f
+  -- 'second' g '.' 'swapped' = 'swapped' '.' 'first' g
+  -- 'bimap' f g '.' 'swapped' = 'swapped' '.' 'bimap' g f
+  -- @
+  --
+  -- >>> (1,2)^.swapped
+  -- (2,1)
+  swapped :: Iso i (p a b) (p c d) (p b a) (p d c)
+
+instance Swapped (,) where
+  swapped = iso swap swap
+  {-# INLINE swapped #-}
+
+instance Swapped Either where
+  swapped = iso (either Right Left) (either Right Left)
+  {-# INLINE swapped #-}
