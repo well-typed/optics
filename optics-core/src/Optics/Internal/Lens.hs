08391e823a6131cab18be3d4d01e4782b9baaf2d
Andrzej Rybczak <andrzej@rybczak.net>
2019-01-13 10:55:52 +0100

    Add withLens

--- optics-core/src/Optics/Internal/Lens.hs
+++ optics-core/src/Optics/Internal/Lens.hs
@@ -29,6 +29,16 @@ lens get set = Optic $
   . first'
 {-# INLINE lens #-}
 
+-- | Work with a lens as a getter and a setter.
+withLens
+  :: Is k A_Lens
+  => Optic k i i s t a b
+  -> ((s -> a) -> (s -> b -> t) -> r)
+  -> r
+withLens o k = case getOptic (toLens o) $ Store id (\_ -> id) of
+  Store get set -> k get set
+{-# INLINE withLens #-}
+
 -- | Build a lens from the van Laarhoven representation.
 lensVL :: LensVL s t a b -> Lens i s t a b
 lensVL l = Optic $
@@ -50,3 +60,18 @@ withLensVL
   -> r
 withLensVL o k = k (toLensVL o)
 {-# INLINE withLensVL #-}
+
+----------------------------------------
+
+-- | Type to represent the components of a lens.
+data Store a b i s t = Store (s -> a) (s -> b -> t)
+
+instance Profunctor (Store a b) where
+  dimap f g (Store get set) = Store (get . f) (\s -> g . set (f s))
+  {-# INLINE dimap #-}
+
+instance Strong (Store a b) where
+  first' (Store get set) = Store (get . fst) (\(s, c) b -> (set s b, c))
+  second' (Store get set) = Store (get . snd) (\(c, s) b -> (c, set s b))
+  {-# INLINE first' #-}
+  {-# INLINE second' #-}
