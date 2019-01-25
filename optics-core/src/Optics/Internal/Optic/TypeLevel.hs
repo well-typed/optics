3c2b65dd9c4316e66e71cfe3ee273458ea811b0f
Andrzej Rybczak <andrzej@rybczak.net>
2019-01-25 05:20:07 +0100

    Move appendProof near (%%)

--- optics-core/src/Optics/Internal/Optic/TypeLevel.hs
+++ optics-core/src/Optics/Internal/Optic/TypeLevel.hs
@@ -1,13 +1,7 @@
 {-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE DataKinds #-}
-{-# LANGUAGE InstanceSigs #-}
 {-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE UndecidableInstances #-}
 module Optics.Internal.Optic.TypeLevel where
-
-import Data.Type.Equality
-import Data.Kind (Type)
-import Data.Proxy (Proxy (..))
 
 -- | Curry a type-level list.
 --
@@ -25,32 +19,6 @@ type family Append (xs :: [*]) (ys :: [*]) :: [*] where
   Append '[]       ys  = ys -- needed for (<%>) and (%>)
   Append xs        '[] = xs -- needed for (<%)
   Append (x ': xs) ys  = x ': Append xs ys
-
--- |
---
--- 'AppendProof' is a very simple class which provides a witness
---
--- @
--- foldr f (foldr f init xs) ys = foldr f init (ys ++ xs)
---    where f = (->)
--- @
---
--- However, 'Append' is unsafe variant of 'AppendProof', i.e. doesn't provide
--- the witness; only calculates and restricts the arguments.
---
-class AppendProof (xs :: [Type]) (ys :: [Type]) (zs :: [Type]) |
-  xs ys -> zs, zs xs -> ys {- , zs ys -> xs -} where
-  appendProof :: Proxy i -> Curry xs (Curry ys i) :~: Curry zs i
-
-instance ys ~ zs => AppendProof '[] ys zs where
-  appendProof _ = Refl
-
-instance AppendProof xs ys zs => AppendProof (x ': xs) ys (x ': zs) where
-  appendProof
-    :: forall i. Proxy i
-    -> Curry (x ': xs) (Curry ys i) :~: Curry (x ': zs) i
-  appendProof i = case appendProof @xs @ys @zs i of
-    Refl -> Refl
 
 class CurryCompose xs where
   composeN :: (i -> j) -> Curry xs i -> Curry xs j
