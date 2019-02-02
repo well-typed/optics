1783719c233d466377b5fba6262d6975125827a5
Andrzej Rybczak <andrzej@rybczak.net>
2019-02-02 13:16:54 +0100

    Add missing Each instances

--- optics-extra/src/Optics/Each.hs
+++ optics-extra/src/Optics/Each.hs
@@ -7,15 +7,31 @@ module Optics.Each
 
 import Data.Array.IArray as IArray
 import Data.Array.Unboxed as Unboxed
+import Data.ByteString as SB
+import Data.ByteString.Lazy as LB
 import Data.Complex
 import Data.Functor.Identity
+import Data.HashMap.Lazy as HashMap
 import Data.IntMap as IntMap
 import Data.List.NonEmpty
 import Data.Map as Map
 import Data.Sequence as Seq
+import Data.Text as ST
+import Data.Text.Lazy as LT
+import Data.Text.Optics (text)
 import Data.Tree as Tree
+import Data.Vector.Generic.Optics (vectorTraverse)
+import Data.Vector.Primitive (Prim)
+import Data.Vector.Storable (Storable)
+import Data.Vector.Unboxed (Unbox)
+import Data.Word
+import qualified Data.Vector as Vector
+import qualified Data.Vector.Primitive as Prim
+import qualified Data.Vector.Storable as Storable
+import qualified Data.Vector.Unboxed as Unboxed
 
-import Optics.Traversal
+import Optics.Core
+import Optics.Extra.Internal.ByteString
 
 -- | Extract 'each' element of a (potentially monomorphic) container.
 --
@@ -80,6 +96,9 @@ instance (c ~ d) => Each (Map c a) (Map d b) a b
 -- | @'each' :: 'Traversal' i ('Map' c a) ('Map' c b) a b@
 instance Each (IntMap a) (IntMap b) a b
 
+-- | @'each' :: 'Traversal' ('HashMap' c a) ('HashMap' c b) a b@
+instance (c ~ d) => Each (HashMap c a) (HashMap d b) a b where
+
 -- | @'each' :: 'Traversal' i [a] [b] a b@
 instance Each [a] [b] a b
 
@@ -97,6 +116,51 @@ instance Each (Seq a) (Seq b) a b
 
 -- | @'each' :: 'Traversal' i ('Tree' a) ('Tree' b) a b@
 instance Each (Tree a) (Tree b) a b
+
+-- | @'each' :: 'Traversal' ('Vector.Vector' a) ('Vector.Vector' b) a b@
+instance Each (Vector.Vector a) (Vector.Vector b) a b where
+  each = noIx vectorTraverse
+  {-# INLINE each #-}
+
+-- | @'each' :: ('Prim' a, 'Prim' b) => 'Traversal' ('Prim.Vector' a)
+-- ('Prim.Vector' b) a b@
+instance (Prim a, Prim b) => Each (Prim.Vector a) (Prim.Vector b) a b where
+  each = noIx vectorTraverse
+  {-# INLINE each #-}
+
+-- | @'each' :: ('Storable' a, 'Storable' b) => 'Traversal' ('Storable.Vector'
+-- a) ('Storable.Vector' b) a b@
+instance (Storable a, Storable b) => Each (Storable.Vector a) (Storable.Vector b) a b where
+  each = noIx vectorTraverse
+  {-# INLINE each #-}
+
+-- | @'each' :: ('Unbox' a, 'Unbox' b) => 'Traversal' ('Unboxed.Vector' a)
+-- ('Unboxed.Vector' b) a b@
+instance (Unbox a, Unbox b) => Each (Unboxed.Vector a) (Unboxed.Vector b) a b where
+  each = noIx vectorTraverse
+  {-# INLINE each #-}
+
+-- | @'each' :: 'Traversal' 'StrictT.Text' 'StrictT.Text' 'Char' 'Char'@
+instance (a ~ Char, b ~ Char) => Each ST.Text ST.Text a b where
+  each = noIx text
+  {-# INLINE each #-}
+
+-- | @'each' :: 'Traversal' 'LazyT.Text' 'LazyT.Text' 'Char' 'Char'@
+instance (a ~ Char, b ~ Char) => Each LT.Text LT.Text a b where
+  each = noIx text
+  {-# INLINE each #-}
+
+-- | @'each' :: 'Traversal' 'StrictB.ByteString' 'StrictB.ByteString' 'Word8'
+-- 'Word8'@
+instance (a ~ Word8, b ~ Word8) => Each SB.ByteString SB.ByteString a b where
+  each = noIx traversedStrictTree
+  {-# INLINE each #-}
+
+-- | @'each' :: 'Traversal' 'LazyB.ByteString' 'LazyB.ByteString' 'Word8'
+-- 'Word8'@
+instance (a ~ Word8, b ~ Word8) => Each LB.ByteString LB.ByteString a b where
+  each = noIx traversedLazy
+  {-# INLINE each #-}
 
 -- | @'each' :: 'Ix' i => 'Traversal' i ('Array' i a) ('Array' i b) a b@
 instance (Ix i, i ~ j) => Each (Array i a) (Array j b) a b where
