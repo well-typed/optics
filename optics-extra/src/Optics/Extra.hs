58fa1c923986da1d5fe7d5a22a525bc6bbce0472
Andrzej Rybczak <andrzej@rybczak.net>
2019-02-05 11:28:08 +0100

    Re-export modules in Optics.Extra

--- optics-extra/src/Optics/Extra.hs
+++ optics-extra/src/Optics/Extra.hs
@@ -1,1 +1,12 @@
-module Optics.Extra where
+module Optics.Extra
+  ( module Optics.Core
+  , module O
+  ) where
+
+import Optics.Core
+
+import Optics.At      as O
+import Optics.Cons    as O
+import Optics.Each    as O
+import Optics.Empty   as O
+import Optics.Indexed as O
