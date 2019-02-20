36fac88ec9aa252cd7372e543817440951600de7
Andrzej Rybczak <andrzej@rybczak.net>
2019-02-20 21:24:19 +0100

    A bit nicer instances for Each

--- optics-core/src/Optics/Each/Core.hs
+++ optics-core/src/Optics/Each/Core.hs
@@ -21,6 +21,9 @@ import Optics.IxTraversal
 -- >>> over each (*10) (1,2,3)
 -- (10,20,30)
 --
+-- >>> iover each (\i a -> a*10 + succ i) (1,2,3)
+-- (11,22,33)
+--
 class Each i s t a b | s -> i a, t -> i b, s b -> t, t a -> s where
   each :: IxTraversal i s t a b
 
@@ -30,109 +33,100 @@ class Each i s t a b | s -> i a, t -> i b, s b -> t, t a -> s where
   each = itraversed
   {-# INLINE each #-}
 
--- | @'each' :: 'IxTraversal' Int (a, a) (b, b) a b@
+-- | @'each' :: 'IxTraversal' 'Int' (a, a) (b, b) a b@
 instance
   (a ~ a1,
-   b ~ b1,
-   i ~ Int
-  ) => Each i (a, a1)
-              (b, b1) a b where
+   b ~ b1
+  ) => Each Int (a, a1)
+                (b, b1) a b where
   each = ixTraversalVL $ \f ~(a0, a1) ->
     (,) <$> f 0 a0 <*> f 1 a1
   {-# INLINE each #-}
 
--- | @'each' :: 'IxTraversal' Int (a, a, a) (b, b, b) a b@
+-- | @'each' :: 'IxTraversal' 'Int' (a, a, a) (b, b, b) a b@
 instance
   (a ~ a1, a ~ a2,
-   b ~ b1, b ~ b2,
-   i ~ Int
-  ) => Each i (a, a1, a2)
-              (b, b1, b2) a b where
+   b ~ b1, b ~ b2
+  ) => Each Int (a, a1, a2)
+                (b, b1, b2) a b where
   each = ixTraversalVL $ \f ~(a0, a1, a2) ->
     (,,) <$> f 0 a0 <*> f 1 a1 <*> f 2 a2
   {-# INLINE each #-}
 
--- | @'each' :: 'IxTraversal' Int (a, a, a, a) (b, b, b, b) a b@
+-- | @'each' :: 'IxTraversal' 'Int' (a, a, a, a) (b, b, b, b) a b@
 instance
   (a ~ a1, a ~ a2, a ~ a3,
-   b ~ b1, b ~ b2, b ~ b3,
-   i ~ Int
-  ) => Each i (a, a1, a2, a3)
-              (b, b1, b2, b3) a b where
+   b ~ b1, b ~ b2, b ~ b3
+  ) => Each Int (a, a1, a2, a3)
+                (b, b1, b2, b3) a b where
   each = ixTraversalVL $ \f ~(a0, a1, a2, a3) ->
     (,,,) <$> f 0 a0 <*> f 1 a1 <*> f 2 a2 <*> f 3 a3
   {-# INLINE each #-}
 
--- | @'each' :: 'IxTraversal' Int (a, a, a, a, a) (b, b, b, b, b) a b@
+-- | @'each' :: 'IxTraversal' 'Int' (a, a, a, a, a) (b, b, b, b, b) a b@
 instance
   (a ~ a1, a ~ a2, a ~ a3, a ~ a4,
-   b ~ b1, b ~ b2, b ~ b3, b ~ b4,
-   i ~ Int
-  ) => Each i (a, a1, a2, a3, a4)
-              (b, b1, b2, b3, b4) a b where
+   b ~ b1, b ~ b2, b ~ b3, b ~ b4
+  ) => Each Int (a, a1, a2, a3, a4)
+                (b, b1, b2, b3, b4) a b where
   each = ixTraversalVL $ \f ~(a0, a1, a2, a3, a4) ->
     (,,,,) <$> f 0 a0 <*> f 1 a1 <*> f 2 a2 <*> f 3 a3 <*> f 4 a4
   {-# INLINE each #-}
 
--- | @'each' :: 'IxTraversal' Int (a, a, a, a, a, a) (b, b, b, b, b, b) a b@
+-- | @'each' :: 'IxTraversal' 'Int' (a, a, a, a, a, a) (b, b, b, b, b, b) a b@
 instance
   (a ~ a1, a ~ a2, a ~ a3, a ~ a4, a ~ a5,
-   b ~ b1, b ~ b2, b ~ b3, b ~ b4, b ~ b5,
-   i ~ Int
-  ) => Each i (a, a1, a2, a3, a4, a5)
-              (b, b1, b2, b3, b4, b5) a b where
+   b ~ b1, b ~ b2, b ~ b3, b ~ b4, b ~ b5
+  ) => Each Int (a, a1, a2, a3, a4, a5)
+                (b, b1, b2, b3, b4, b5) a b where
   each = ixTraversalVL $ \f ~(a0, a1, a2, a3, a4, a5) ->
     (,,,,,) <$> f 0 a0 <*> f 1 a1 <*> f 2 a2 <*> f 3 a3 <*> f 4 a4
             <*> f 5 a5
   {-# INLINE each #-}
 
--- | @'each' :: 'IxTraversal' Int (a, a, a, a, a, a, a) (b, b, b, b, b, b, b) a
+-- | @'each' :: 'IxTraversal' 'Int' (a, a, a, a, a, a, a) (b, b, b, b, b, b, b) a
 -- b@
 instance
   (a ~ a1, a ~ a2, a ~ a3, a ~ a4, a ~ a5, a ~ a6,
-   b ~ b1, b ~ b2, b ~ b3, b ~ b4, b ~ b5, b ~ b6,
-   i ~ Int
-  ) => Each i (a, a1, a2, a3, a4, a5, a6)
-              (b, b1, b2, b3, b4, b5, b6) a b where
+   b ~ b1, b ~ b2, b ~ b3, b ~ b4, b ~ b5, b ~ b6
+  ) => Each Int (a, a1, a2, a3, a4, a5, a6)
+                (b, b1, b2, b3, b4, b5, b6) a b where
   each = ixTraversalVL $ \f ~(a0, a1, a2, a3, a4, a5, a6) ->
     (,,,,,,) <$> f 0 a0 <*> f 1 a1 <*> f 2 a2 <*> f 3 a3 <*> f 4 a4
              <*> f 5 a5 <*> f 6 a6
   {-# INLINE each #-}
 
--- | @'each' :: 'IxTraversal' Int (a, a, a, a, a, a, a, a) (b, b, b, b, b, b, b,
+-- | @'each' :: 'IxTraversal' 'Int' (a, a, a, a, a, a, a, a) (b, b, b, b, b, b, b,
 -- b) a b@
 instance
   (a ~ a1, a ~ a2, a ~ a3, a ~ a4, a ~ a5, a ~ a6, a ~ a7,
-   b ~ b1, b ~ b2, b ~ b3, b ~ b4, b ~ b5, b ~ b6, b ~ b7,
-   i ~ Int
-  ) => Each i (a, a1, a2, a3, a4, a5, a6, a7)
-              (b, b1, b2, b3, b4, b5, b6, b7) a b where
+   b ~ b1, b ~ b2, b ~ b3, b ~ b4, b ~ b5, b ~ b6, b ~ b7
+  ) => Each Int (a, a1, a2, a3, a4, a5, a6, a7)
+                (b, b1, b2, b3, b4, b5, b6, b7) a b where
   each = ixTraversalVL $ \f ~(a0, a1, a2, a3, a4, a5, a6, a7) ->
     (,,,,,,,) <$> f 0 a0 <*> f 1 a1 <*> f 2 a2 <*> f 3 a3 <*> f 4 a4
               <*> f 5 a5 <*> f 6 a6 <*> f 7 a7
   {-# INLINE each #-}
 
--- | @'each' :: 'Traversal' i (a, a, a, a, a, a, a, a, a) (b, b, b, b, b, b, b,
--- b, b) a b@
+-- | @'each' :: 'Traversal' 'Int' (a, a, a, a, a, a, a, a, a) (b, b, b, b, b, b,
+-- b, b, b) a b@
 instance
   (a ~ a1, a ~ a2, a ~ a3, a ~ a4, a ~ a5, a ~ a6, a ~ a7, a ~ a8,
-   b ~ b1, b ~ b2, b ~ b3, b ~ b4, b ~ b5, b ~ b6, b ~ b7, b ~ b8,
-   i ~ Int
-  ) => Each i (a, a1, a2, a3, a4, a5, a6, a7, a8)
-              (b, b1, b2, b3, b4, b5, b6, b7, b8) a b where
+   b ~ b1, b ~ b2, b ~ b3, b ~ b4, b ~ b5, b ~ b6, b ~ b7, b ~ b8
+  ) => Each Int (a, a1, a2, a3, a4, a5, a6, a7, a8)
+                (b, b1, b2, b3, b4, b5, b6, b7, b8) a b where
   each = ixTraversalVL $ \f ~(a0, a1, a2, a3, a4, a5, a6, a7, a8) ->
     (,,,,,,,,) <$> f 0 a0 <*> f 1 a1 <*> f 2 a2 <*> f 3 a3 <*> f 4 a4
                <*> f 5 a5 <*> f 6 a6 <*> f 7 a7 <*> f 8 a8
   {-# INLINE each #-}
 
--- | @'each' :: 'Traversal' i (a, a, a, a, a, a, a, a, a, a) (b, b, b, b, b, b,
--- b, b, b, b) a b@
+-- | @'each' :: 'Traversal' 'Int' (a, a, a, a, a, a, a, a, a, a) (b, b, b, b, b,
+-- b, b, b, b, b) a b@
 instance
   (a ~ a1, a ~ a2, a ~ a3, a ~ a4, a ~ a5, a ~ a6, a ~ a7, a ~ a8, a ~ a9,
-   b ~ b1, b ~ b2, b ~ b3, b ~ b4, b ~ b5, b ~ b6, b ~ b7, b ~ b8, b ~ b9,
-   i ~ Int
-  ) => Each i (a, a1, a2, a3, a4, a5, a6, a7, a8, a9)
-              (b, b1, b2, b3, b4, b5, b6, b7, b8, b9) a b where
+   b ~ b1, b ~ b2, b ~ b3, b ~ b4, b ~ b5, b ~ b6, b ~ b7, b ~ b8, b ~ b9
+  ) => Each Int (a, a1, a2, a3, a4, a5, a6, a7, a8, a9)
+                (b, b1, b2, b3, b4, b5, b6, b7, b8, b9) a b where
   each = ixTraversalVL $ \f ~(a0, a1, a2, a3, a4, a5, a6, a7, a8, a9) ->
     (,,,,,,,,,) <$> f 0 a0 <*> f 1 a1 <*> f 2 a2 <*> f 3 a3 <*> f 4 a4
                 <*> f 5 a5 <*> f 6 a6 <*> f 7 a7 <*> f 8 a8 <*> f 9 a9
@@ -140,33 +134,33 @@ instance
 
 -- | @'each' :: ('RealFloat' a, 'RealFloat' b) => 'IxTraversal' (Either () ())
 -- ('Complex' a) ('Complex' b) a b@
-instance i ~ Either () () => Each i (Complex a) (Complex b) a b where
+instance Each (Either () ()) (Complex a) (Complex b) a b where
   each = ixTraversalVL $ \f (a :+ b) -> (:+) <$> f (Left ()) a <*> f (Right ()) b
   {-# INLINE each #-}
 
--- | @'each' :: 'Traversal' k ('Map' k a) ('Map' k b) a b@
+-- | @'each' :: 'IxTraversal' k ('Map' k a) ('Map' k b) a b@
 instance k ~ k' => Each k (Map k a) (Map k' b) a b
 
--- | @'each' :: 'Traversal' i ('Map' c a) ('Map' c b) a b@
-instance i ~ Int => Each i (IntMap a) (IntMap b) a b
+-- | @'each' :: 'IxTraversal' 'Int' ('IntMap' a) ('IntMap' b) a b@
+instance Each Int (IntMap a) (IntMap b) a b
 
--- | @'each' :: 'Traversal' i [a] [b] a b@
-instance i ~ Int => Each i [a] [b] a b
+-- | @'each' :: 'IxTraversal' 'Int' [a] [b] a b@
+instance Each Int [a] [b] a b
 
--- | @'each' :: 'IxTraversal' Int (NonEmpty a) (NonEmpty b) a b@
-instance i ~ Int => Each i (NonEmpty a) (NonEmpty b) a b
+-- | @'each' :: 'IxTraversal' 'Int' (NonEmpty a) (NonEmpty b) a b@
+instance Each Int (NonEmpty a) (NonEmpty b) a b
 
 -- | @'each' :: 'IxTraversal' () ('Identity' a) ('Identity' b) a b@
-instance i ~ () => Each i (Identity a) (Identity b) a b
+instance Each () (Identity a) (Identity b) a b
 
 -- | @'each' :: 'IxTraversal' () ('Maybe' a) ('Maybe' b) a b@
-instance i ~ () => Each i (Maybe a) (Maybe b) a b
+instance Each () (Maybe a) (Maybe b) a b
 
--- | @'each' :: 'IxTraversal' Int ('Seq' a) ('Seq' b) a b@
-instance i ~ Int => Each i (Seq a) (Seq b) a b
+-- | @'each' :: 'IxTraversal' 'Int' ('Seq' a) ('Seq' b) a b@
+instance Each Int (Seq a) (Seq b) a b
 
 -- | @'each' :: 'IxTraversal' [Int] ('Tree' a) ('Tree' b) a b@
-instance i ~ [Int] => Each i (Tree a) (Tree b) a b
+instance Each [Int] (Tree a) (Tree b) a b
 
 -- | @'each' :: 'Ix' i => 'IxTraversal' i ('Array' i a) ('Array' i b) a b@
 instance (Ix i, i ~ j) => Each i (Array i a) (Array j b) a b
--- optics-extra/src/Optics/Each.hs
+++ optics-extra/src/Optics/Each.hs
@@ -18,10 +18,10 @@ import Data.Vector.Primitive (Prim)
 import Data.Vector.Storable (Storable)
 import Data.Vector.Unboxed (Unbox)
 import Data.Word
-import qualified Data.Vector as Vector
-import qualified Data.Vector.Primitive as Prim
-import qualified Data.Vector.Storable as Storable
-import qualified Data.Vector.Unboxed as Unboxed
+import qualified Data.Vector as V
+import qualified Data.Vector.Primitive as VP
+import qualified Data.Vector.Storable as VS
+import qualified Data.Vector.Unboxed as VU
 
 import Optics.Core
 import Optics.Indexed ()
@@ -32,61 +32,47 @@ import Optics.Extra.Internal.ByteString
 -- | @'each' :: 'IxTraversal' k ('HashMap' k a) ('HashMap' k b) a b@
 instance k ~ k' => Each k (HashMap k a) (HashMap k' b) a b where
 
--- | @'each' :: 'IxTraversal' Int ('Vector.Vector' a) ('Vector.Vector' b) a b@
-instance i ~ Int => Each i (Vector.Vector a) (Vector.Vector b) a b where
+-- | @'each' :: 'IxTraversal' Int ('V.Vector' a) ('V.Vector' b) a b@
+instance Each Int (V.Vector a) (V.Vector b) a b where
   each = vectorTraverse
   {-# INLINE each #-}
 
 -- | @'each' :: ('Prim' a, 'Prim' b) => 'IxTraversal' Int ('Prim.Vector' a)
 -- ('Prim.Vector' b) a b@
-instance
-  (Prim a, Prim b, i ~ Int
-  ) => Each i (Prim.Vector a) (Prim.Vector b) a b where
+instance (Prim a, Prim b) => Each Int (VP.Vector a) (VP.Vector b) a b where
   each = vectorTraverse
   {-# INLINE each #-}
 
--- | @'each' :: ('Storable' a, 'Storable' b) => 'Traversal' Int ('Storable.Vector'
--- a) ('Storable.Vector' b) a b@
-instance
-  (Storable a, Storable b, i ~ Int
-  ) => Each i (Storable.Vector a) (Storable.Vector b) a b where
+-- | @'each' :: ('Storable' a, 'Storable' b) => 'Traversal' 'Int' ('VS.Vector'
+-- a) ('VS.Vector' b) a b@
+instance (Storable a, Storable b) => Each Int (VS.Vector a) (VS.Vector b) a b where
   each = vectorTraverse
   {-# INLINE each #-}
 
--- | @'each' :: ('Unbox' a, 'Unbox' b) => 'IxTraversal' Int ('Unboxed.Vector' a)
--- ('Unboxed.Vector' b) a b@
-instance
-  (Unbox a, Unbox b, i ~ Int
-  ) => Each i (Unboxed.Vector a) (Unboxed.Vector b) a b where
+-- | @'each' :: ('Unbox' a, 'Unbox' b) => 'IxTraversal' 'Int' ('VU.Vector' a)
+-- ('VU.Vector' b) a b@
+instance (Unbox a, Unbox b ) => Each Int (VU.Vector a) (VU.Vector b) a b where
   each = vectorTraverse
   {-# INLINE each #-}
 
--- | @'each' :: 'Traversal' 'StrictT.Text' 'StrictT.Text' 'Char' 'Char'@
-instance
-  (a ~ Char, b ~ Char, i ~ Int
-  ) => Each i ST.Text ST.Text a b where
+-- | @'each' :: 'IxTraversal' 'Int' 'ST.Text' 'ST.Text' 'Char' 'Char'@
+instance (a ~ Char, b ~ Char) => Each Int ST.Text ST.Text a b where
   each = text
   {-# INLINE each #-}
 
--- | @'each' :: 'Traversal' 'LazyT.Text' 'LazyT.Text' 'Char' 'Char'@
-instance
-  (a ~ Char, b ~ Char, i ~ Int
-  ) => Each i LT.Text LT.Text a b where
+-- | @'each' :: 'IxTraversal' 'Int64' 'LT.Text' 'LT.Text' 'Char' 'Char'@
+instance (a ~ Char, b ~ Char) => Each Int LT.Text LT.Text a b where
   each = text
   {-# INLINE each #-}
 
--- | @'each' :: 'IxTraversal' 'Int' 'StrictB.ByteString' 'StrictB.ByteString' 'Word8'
+-- | @'each' :: 'IxTraversal' 'Int' 'SB.ByteString' 'SB.ByteString' 'Word8'
 -- 'Word8'@
-instance
-  (a ~ Word8, b ~ Word8, i ~ Int
-  ) => Each i SB.ByteString SB.ByteString a b where
+instance (a ~ Word8, b ~ Word8) => Each Int SB.ByteString SB.ByteString a b where
   each = traversedStrictTree
   {-# INLINE each #-}
 
--- | @'each' :: 'IxTraversal' 'Int64' 'LazyB.ByteString' 'LazyB.ByteString'
--- 'Word8' 'Word8'@
-instance
-  (a ~ Word8, b ~ Word8, i ~ Int64
-  ) => Each i LB.ByteString LB.ByteString a b where
+-- | @'each' :: 'IxTraversal' 'Int64' 'LB.ByteString' 'LB.ByteString' 'Word8'
+-- 'Word8'@
+instance (a ~ Word8, b ~ Word8) => Each Int64 LB.ByteString LB.ByteString a b where
   each = traversedLazy
   {-# INLINE each #-}
