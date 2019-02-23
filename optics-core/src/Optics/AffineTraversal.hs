7ecad3e6bf94a0e72eff7dc12cf2ee07fed1c344
Andrzej Rybczak <andrzej@rybczak.net>
2019-02-23 20:53:19 +0100

    Remove atraversal'

--- optics-core/src/Optics/AffineTraversal.hs
+++ optics-core/src/Optics/AffineTraversal.hs
@@ -4,7 +4,6 @@ module Optics.AffineTraversal
   , AffineTraversal'
   , toAffineTraversal
   , atraversal
-  , atraversal'
   , withAffineTraversal
   , AffineTraversalVL
   , AffineTraversalVL'
@@ -56,11 +55,6 @@ atraversal match update = Optic $
   . first'
   . right'
 {-# INLINE atraversal #-}
-
--- | Build a type-preserving affine traversal from a matcher and an updater.
-atraversal' :: (s -> Maybe a) -> (s -> b -> s) -> AffineTraversal s s a b
-atraversal' sma sbs = atraversal (\s -> maybe (Left s) Right (sma s)) sbs
-{-# INLINE atraversal' #-}
 
 -- With with an affine traversal as a matcher and an updater.
 withAffineTraversal
