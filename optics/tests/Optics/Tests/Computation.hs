c58031e9a176262f823d47c2c7ebc7b2bba6c30d
Andrzej Rybczak <andrzej@rybczak.net>
2019-02-24 22:29:35 +0100

    Rename functions in Tests.Computation and test afolding

--- optics/tests/Optics/Tests/Computation.hs
+++ optics/tests/Optics/Tests/Computation.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# OPTIONS_GHC -fplugin=Test.Inspection.Plugin -dsuppress-all #-}
-module Optics.Tests.Computation where
+module Optics.Tests.Computation (computationTests) where
 
 import Test.Tasty
 import Test.Tasty.HUnit
@@ -11,47 +11,54 @@ import Optics.Tests.Utils
 
 computationTests :: TestTree
 computationTests = testGroup "computation"
-    [ testGroup "Lens"
-        [ testCase "view (lens f g) = f" $
-            assertSuccess $(inspectTest $ 'compL01 === 'compR01)
-        , testCase "set (lens f g) = g" $
-            assertSuccess $(inspectTest $ 'compL02 === 'compR02)
-        ]
-    , testGroup "AffineTraversal"
-        -- this doesn't hold definitionally: we need law here
-        [ testCase "withAffineTraversal (atraversal f g) (\\ _ g' -> g') /= g" $
-             assertFailure' $(inspectTest $ 'compL03 === 'compR03)
-        , testCase "withAffineTraversal (atraversal f g) (\\ _ g' -> g') = ..." $
-             assertSuccess $(inspectTest $ 'compL03 ==- 'compR03_)
-        , testCase "withAffineTraversal (atraversal f g) (\\ f' _ -> f') = f" $
-            assertSuccess $(inspectTest $ 'compL04 === 'compR04)
-        ]
-
-    , testGroup "Setter"
-        [ testCase "over (sets f) = f" $
-            assertSuccess $(inspectTest $ 'compL05 === 'compR05)
-        ]
+  [ testGroup "Lens"
+    [ testCase "view (lens f g) = f" $
+      assertSuccess $(inspectTest $ 'lens1lhs === 'lens1rhs)
+    , testCase "set (lens f g) = g" $
+      assertSuccess $(inspectTest $ 'lens2lhs === 'lens2rhs)
     ]
+  , testGroup "AffineTraversal"
+    -- this doesn't hold definitionally: we need law here
+    [ testCase "withAffineTraversal (atraversal f g) (\\ _ g' -> g') /= g" $
+      assertFailure' $(inspectTest $ 'atraversal1lhs === 'atraversal1rhs)
+    , testCase "withAffineTraversal (atraversal f g) (\\ _ g' -> g') = ..." $
+      assertSuccess $(inspectTest $ 'atraversal1lhs === 'atraversal1rhs_)
+    , testCase "withAffineTraversal (atraversal f g) (\\ f' _ -> f') = f" $
+      assertSuccess $(inspectTest $ 'atraversal2lhs === 'atraversal2rhs)
+    ]
+  , testGroup "AffineFold"
+    [ testCase "preview (afolding f) = f" $
+      assertSuccess $(inspectTest $ 'afold1lhs === 'afold1rhs)
+    ]
+  , testGroup "Setter"
+    [ testCase "over (sets f) = f" $
+      assertSuccess $(inspectTest $ 'setter1lhs === 'setter1rhs)
+    ]
+  ]
 
-compL01, compR01 :: (s -> a) -> (s -> a -> s) -> (s -> a)
-compL01 f g s = view (lens f g) s
-compR01 f _ s = f s
+lens1lhs, lens1rhs :: (s -> a) -> (s -> a -> s) -> (s -> a)
+lens1lhs f g s = view (lens f g) s
+lens1rhs f _ s = f s
 
-compL02, compR02 :: (s -> a) -> (s -> b -> t) -> (s -> b -> t)
-compL02 f g s b = set (lens f g) b s
-compR02 _ g s b = g s b
+lens2lhs, lens2rhs :: (s -> a) -> (s -> b -> t) -> (s -> b -> t)
+lens2lhs f g s b = set (lens f g) b s
+lens2rhs _ g s b = g s b
 
-compL03, compR03, compR03_ :: (s -> Either t a) -> (s -> b -> t) -> (s -> b -> t)
-compL03 f g s b = withAffineTraversal (atraversal f g) (\_ g' -> g') s b
-compR03 _ g s b = g s b
-compR03_ f g s b = case f s of
-    Left t  -> t
-    Right _ -> g s b
+atraversal1lhs, atraversal1rhs, atraversal1rhs_
+  :: (s -> Either t a) -> (s -> b -> t) -> (s -> b -> t)
+atraversal1lhs f g s b = withAffineTraversal (atraversal f g) (\_ g' -> g') s b
+atraversal1rhs _ g s b = g s b
+atraversal1rhs_ f g s b = either id (\_ -> g s b) (f s)
 
-compL04, compR04 :: (s -> Either t a) -> (s -> b -> t) -> (s -> Either t a)
-compL04 f g s = withAffineTraversal (atraversal f g) (\f' _ -> f') s
-compR04 f _ s = f s
+atraversal2lhs, atraversal2rhs
+  :: (s -> Either t a) -> (s -> b -> t) -> (s -> Either t a)
+atraversal2lhs f g s = withAffineTraversal (atraversal f g) (\f' _ -> f') s
+atraversal2rhs f _ s = f s
 
-compL05, compR05 :: ((a -> b) -> s -> t) -> ((a -> b) -> s -> t)
-compL05 f ab s = over (sets f) ab s
-compR05 f ab s = f ab s
+afold1lhs, afold1rhs :: (s -> Maybe a) -> s -> Maybe a
+afold1lhs sma s = preview (afolding sma) s
+afold1rhs sma s = sma s
+
+setter1lhs, setter1rhs :: ((a -> b) -> s -> t) -> ((a -> b) -> s -> t)
+setter1lhs f ab s = over (sets f) ab s
+setter1rhs f ab s = f ab s
