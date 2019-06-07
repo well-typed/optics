12cf935a13f0f66d581644cfa8ea624e7cda1447
Andrzej Rybczak <andrzej@rybczak.net>
2019-06-07 15:19:07 +0200

    Add example usage and get rid of ReadOnlyOptic type

--- optics-core/src/Optics/ReadOnly.hs
+++ optics-core/src/Optics/ReadOnly.hs
@@ -8,32 +8,47 @@ import Optics.Internal.Profunctor
 
 -- | Class for read-write optics that have their read-only counterparts.
 class ToReadOnly k where
-  type ReadOnlyOptic k
   -- | Turn read-write optic into its read-only counterpart.
-  getting :: Optic k is s t a b -> Optic' (ReadOnlyOptic k) is s a
+  --
+  -- This is useful when you have an @optic :: Optic k is s t a b@ of read-write
+  -- kind @k@ such that @s@, @t@, @a@, @b@ are rigid, there is no evidence that
+  -- @s ~ t@ and @a ~ b@ and you want to pass @optic@ to one of the functions
+  -- that accept read-only optic kinds.
+  --
+  -- Example:
+  --
+  -- @
+  -- λ> let fstIntToChar = _1 :: Lens (Int, r) (Char, r) Int Char
+  -- λ> :t view fstIntToChar
+  --
+  -- <interactive>:1:6: error:
+  --     • Couldn't match type ‘Char’ with ‘Int’
+  --       Expected type: Optic' A_Lens NoIx (Int, r) Int
+  --         Actual type: Lens (Int, r) (Char, r) Int Char
+  --     • In the first argument of ‘view’, namely ‘fstIntToChar’
+  --       In the expression: view fstIntToChar
+  -- λ> :t view (getting fstIntToChar)
+  -- view (getting fstIntToChar) :: (Int, r) -> Int
+  -- @
+  getting :: Optic k is s t a b -> Optic' (Join A_Getter k) is s a
 
 instance ToReadOnly An_Iso where
-  type ReadOnlyOptic An_Iso = A_Getter
   getting o = Optic (getting__ o)
   {-# INLINE getting #-}
 
 instance ToReadOnly A_Lens where
-  type ReadOnlyOptic A_Lens = A_Getter
   getting o = Optic (getting__ o)
   {-# INLINE getting #-}
 
 instance ToReadOnly A_Prism where
-  type ReadOnlyOptic A_Prism = An_AffineFold
   getting o = Optic (getting__ o)
   {-# INLINE getting #-}
 
 instance ToReadOnly An_AffineTraversal where
-  type ReadOnlyOptic An_AffineTraversal = An_AffineFold
   getting o = Optic (getting__ o)
   {-# INLINE getting #-}
 
 instance ToReadOnly A_Traversal where
-  type ReadOnlyOptic A_Traversal = A_Fold
   getting o = Optic (getting__ o)
   {-# INLINE getting #-}
 
