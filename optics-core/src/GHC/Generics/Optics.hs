a9fa7ed47900317df39a4802d972cc18d6490f78
Andrzej Rybczak <andrzej@rybczak.net>
2019-06-08 22:32:10 +0200

    Generalize type signature of generic1

--- optics-core/src/GHC/Generics/Optics.hs
+++ optics-core/src/GHC/Generics/Optics.hs
@@ -49,7 +49,7 @@ generic = iso GHC.from GHC.to
 {-# INLINE generic #-}
 
 -- | Convert from the data type to its representation (or back)
-generic1 :: Generic1 f => Iso' (f a) (Rep1 f a)
+generic1 :: Generic1 f => Iso (f a) (f b) (Rep1 f a) (Rep1 f b)
 generic1 = iso GHC.from1 GHC.to1
 {-# INLINE generic1 #-}
 
