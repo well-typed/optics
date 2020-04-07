0422f49e1beb033ea495cfd9a07121da4a5cd995
Andrzej Rybczak <andrzej@rybczak.net>
2020-04-07 05:52:00 +0200

    Provide conversions from Iso/Prism to their VL representation

--- optics-vl/src/Optics/VL.hs
+++ optics-vl/src/Optics/VL.hs
@@ -1,10 +1,11 @@
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
 -- |
 -- Module: Optics.VL
 --
--- This module provides compatibility layer for converting from van Laarhoven
+-- This module provides compatibility layer for converting from/to van Laarhoven
 -- encoding of 'Iso's, 'Prism's, 'Lens'es, 'IxLens'es, 'AffineTraversal's,
 -- 'IxAffineTraversal's, 'Traversal's and 'IxTraversal's to their optics
 -- equivalents.
@@ -14,58 +15,97 @@ module Optics.VL
     IsoVL
   , IsoVL'
   , isoVL
+  , toIsoVL
+  , withIsoVL
   -- * Prism
   , PrismVL
   , PrismVL'
   , prismVL
+  , toPrismVL
+  , withPrismVL
   -- * Lens
   , LensVL
   , LensVL'
   , lensVL
+  , toLensVL
+  , withLensVL
   -- * IxLens
   , IxLensVL
   , IxLensVL'
   , ilensVL
+  , toIxLensVL
+  , withIxLensVL
   -- * AffineTraversal
   , AffineTraversalVL
   , AffineTraversalVL'
   , atraversalVL
+  , atraverseOf
   -- * IxAffineTraversal
   , IxAffineTraversalVL
   , IxAffineTraversalVL'
   , iatraversalVL
+  , iatraverseOf
   -- * Traversal
   , TraversalVL
   , TraversalVL'
   , traversalVL
+  , traverseOf
   -- * IxTraversal
   , IxTraversalVL
   , IxTraversalVL'
   , itraversalVL
+  , itraverseOf
   ) where
 
+import Data.Coerce
 import Data.Functor.Identity
+import Data.Profunctor.Indexed ((.#), (#.))
 import qualified Data.Profunctor as P
-
-import Data.Profunctor.Indexed
+import qualified Data.Profunctor.Indexed as IP
 
 import Optics.Internal.Optic
 import Optics.Core
 
-newtype WrappedProfunctor p i a b =
-  WrapProfunctor { unwrapProfunctor :: p i a b }
+newtype WrappedIxProfunctor p i a b =
+  WrapIxProfunctor { unwrapIxProfunctor :: p i a b }
 
-instance Profunctor p => P.Profunctor (WrappedProfunctor p i) where
-  dimap f g (WrapProfunctor pab) = WrapProfunctor (dimap f g pab)
-  lmap  f   (WrapProfunctor pab) = WrapProfunctor (lmap  f   pab)
-  rmap    g (WrapProfunctor pab) = WrapProfunctor (rmap    g pab)
+instance IP.Profunctor p => P.Profunctor (WrappedIxProfunctor p i) where
+  dimap f g (WrapIxProfunctor piab) = WrapIxProfunctor (IP.dimap f g piab)
+  lmap  f   (WrapIxProfunctor piab) = WrapIxProfunctor (IP.lmap  f   piab)
+  rmap    g (WrapIxProfunctor piab) = WrapIxProfunctor (IP.rmap    g piab)
   {-# INLINE dimap #-}
   {-# INLINE lmap #-}
   {-# INLINE rmap #-}
 
-instance Choice p => P.Choice (WrappedProfunctor p i) where
-  left'  (WrapProfunctor pab) = WrapProfunctor (left'  pab)
-  right' (WrapProfunctor pab) = WrapProfunctor (right' pab)
+instance IP.Choice p => P.Choice (WrappedIxProfunctor p i) where
+  left'  (WrapIxProfunctor piab) = WrapIxProfunctor (IP.left'  piab)
+  right' (WrapIxProfunctor piab) = WrapIxProfunctor (IP.right' piab)
+  {-# INLINE left' #-}
+  {-# INLINE right' #-}
+
+----------------------------------------
+
+newtype WrappedProfunctor p f i a b =
+  WrapProfunctor { unwrapProfunctor :: p a (f b) }
+
+instance (P.Profunctor p, Functor f) => IP.Profunctor (WrappedProfunctor p f) where
+  dimap f g (WrapProfunctor pafb) = WrapProfunctor (P.dimap f (fmap g) pafb)
+  lmap  f   (WrapProfunctor pafb) = WrapProfunctor (P.lmap  f          pafb)
+  rmap    g (WrapProfunctor pafb) = WrapProfunctor (P.rmap    (fmap g) pafb)
+  {-# INLINE dimap #-}
+  {-# INLINE lmap #-}
+  {-# INLINE rmap #-}
+
+  lcoerce' = IP.lmap coerce
+  rcoerce' = IP.rmap coerce
+  {-# INLINE lcoerce' #-}
+  {-# INLINE rcoerce' #-}
+
+instance (P.Choice p, Applicative f) => IP.Choice (WrappedProfunctor p f) where
+  left' (WrapProfunctor pafb) =
+    WrapProfunctor (P.rmap (either (fmap Left) (pure . Right)) (P.left' pafb))
+  right' (WrapProfunctor pafb) =
+    WrapProfunctor (P.rmap (either (pure . Left) (fmap Right)) (P.right' pafb))
   {-# INLINE left' #-}
   {-# INLINE right' #-}
 
@@ -80,10 +120,24 @@ type IsoVL' s a = IsoVL s s a a
 
 -- | Build an 'Iso' from the van Laarhoven representation.
 isoVL :: forall s t a b. IsoVL s t a b -> Iso s t a b
-isoVL f = Optic $ rcoerce @(Identity t) @t
-                . (unwrapProfunctor #. f .# WrapProfunctor)
-                . rcoerce @b @(Identity b)
+isoVL f = Optic $ IP.rcoerce @(Identity t) @t
+                . (unwrapIxProfunctor #. f .# WrapIxProfunctor)
+                . IP.rcoerce @b @(Identity b)
 {-# INLINE isoVL #-}
+
+-- | Convert an 'Iso' to the van Laarhoven representation.
+toIsoVL :: Is k An_Iso => Optic k is s t a b -> IsoVL s t a b
+toIsoVL o = unwrapProfunctor #. getOptic (castOptic @An_Iso o) .# WrapProfunctor
+{-# INLINE toIsoVL #-}
+
+-- | Work with an 'Iso' in the van Laarhoven representation.
+withIsoVL
+  :: Is k An_Iso
+  => Optic k is s t a b
+  -> (IsoVL s t a b -> r)
+  -> r
+withIsoVL o k = k (toIsoVL o)
+{-# INLINE withIsoVL #-}
 
 ----------------------------------------
 
@@ -96,7 +150,21 @@ type PrismVL' s a = PrismVL s s a a
 
 -- | Build a 'Prism' from the van Laarhoven representation.
 prismVL :: forall s t a b. PrismVL s t a b -> Prism s t a b
-prismVL f = Optic $ rcoerce @(Identity t) @t
-                  . (unwrapProfunctor #. f .# WrapProfunctor)
-                  . rcoerce @b @(Identity b)
+prismVL f = Optic $ IP.rcoerce @(Identity t) @t
+                  . (unwrapIxProfunctor #. f .# WrapIxProfunctor)
+                  . IP.rcoerce @b @(Identity b)
 {-# INLINE prismVL #-}
+
+-- | Convert a 'Prism' to the van Laarhoven representation.
+toPrismVL :: Is k A_Prism => Optic k is s t a b -> PrismVL s t a b
+toPrismVL o = unwrapProfunctor #. getOptic (castOptic @A_Prism o) .# WrapProfunctor
+{-# INLINE toPrismVL #-}
+
+-- | Work with a 'Prism' in the van Laarhoven representation.
+withPrismVL
+  :: Is k A_Prism
+  => Optic k is s t a b
+  -> (PrismVL s t a b -> r)
+  -> r
+withPrismVL o k = k (toPrismVL o)
+{-# INLINE withPrismVL #-}
