cd209de4bb18839c22ed6f44bbf92b12efbd92a2
Andrzej Rybczak <andrzej@rybczak.net>
2020-09-17 01:31:22 +0200

    Tidy up AppendIndices

--- optics-core/src/Optics/Internal/Optic.hs
+++ optics-core/src/Optics/Internal/Optic.hs
@@ -38,8 +38,6 @@ module Optics.Internal.Optic
   ) where
 
 import Data.Function ((&))
-import Data.Kind (Type)
-import Data.Proxy (Proxy (..))
 import Data.Type.Equality
 import GHC.Generics (Rep)
 import GHC.OverloadedLabels
@@ -50,9 +48,6 @@ import Data.Profunctor.Indexed
 import Optics.Internal.Optic.Subtyping
 import Optics.Internal.Optic.TypeLevel
 import Optics.Internal.Optic.Types
-
--- to make %% simpler
-import Unsafe.Coerce (unsafeCoerce)
 
 -- | Wrapper newtype for the whole family of optics.
 --
@@ -150,14 +145,8 @@ infixl 9 %%
      -> Optic k ks s t a b
 Optic o %% Optic o' = Optic oo
   where
-    -- unsafeCoerce to the rescue, for a proof see below.
     oo :: forall p i. (Profunctor p, Constraints k p) => Optic__ p i (Curry ks i) s t a b
-    oo = (unsafeCoerce
-           :: Optic__ p i (Curry is (Curry js i)) s t a b
-           -> Optic__ p i (Curry ks i           ) s t a b)
-      ( (o  :: Optic__ p (Curry js i) (Curry is (Curry js i)) s t u v)
-      . (o' :: Optic__ p i            (Curry js i)            u v a b)
-      )
+    oo | Refl <- appendIndices @is @js @i = o . o'
 {-# INLINE (%%) #-}
 
 -- | Flipped function application, specialised to optics and binding tightly.
@@ -173,34 +162,6 @@ infixl 9 %&
      -> Optic l js s' t' a' b'
 (%&) = (&)
 {-# INLINE (%&) #-}
-
-
--- |
---
--- 'AppendProof' is a very simple class which provides a witness
---
--- @
--- foldr f (foldr f init xs) ys = foldr f init (ys ++ xs)
---    where f = (->)
--- @
---
--- It shows that usage of 'unsafeCoerce' in '(%%)' is, in fact, safe.
---
-class Append xs ys ~ zs => AppendProof (xs :: [Type]) (ys :: [Type]) (zs :: [Type])
-  | xs ys -> zs, zs xs -> ys {- , zs ys -> xs -} where
-  appendProof :: Proxy i -> Curry xs (Curry ys i) :~: Curry zs i
-
-instance ys ~ zs => AppendProof '[] ys zs where
-  appendProof _ = Refl
-
-instance
-  (Append (x : xs) ys ~ (x : zs), AppendProof xs ys zs
-  ) => AppendProof (x ': xs) ys (x ': zs) where
-  appendProof
-    :: forall i. Proxy i
-    -> Curry (x ': xs) (Curry ys i) :~: Curry (x ': zs) i
-  appendProof i = case appendProof @xs @ys @zs i of
-    Refl -> Refl
 
 ----------------------------------------
 -- Labels
