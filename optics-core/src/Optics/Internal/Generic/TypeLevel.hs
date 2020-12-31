53d463d22e1c3596ee042cd07a8bc39981a52c2b
Andrzej Rybczak <arybczak@users.noreply.github.com>
2020-12-31 15:26:43 +0100

    Tidy Generic internals (#386)

--- optics-core/src/Optics/Internal/Generic/TypeLevel.hs
+++ optics-core/src/Optics/Internal/Generic/TypeLevel.hs
@@ -5,8 +5,23 @@
 
 -- | This module is intended for internal use only, and may change without
 -- warning in subsequent releases.
-module Optics.Internal.Generic.TypeLevel where
+module Optics.Internal.Generic.TypeLevel
+  ( -- * Pathing
+    PathTree(..)
+  , Path(..)
+  -- ** Names
+  , GetFieldPaths
+  , GetNamePath
+  -- ** Positions
+  , GetPositionPaths
+  , GetPositionPath
+  -- * Misc
+  , HideReps
+  , AnyHasPath
+  , NoGenericError
+  ) where
 
+import Data.Kind
 import Data.Type.Bool
 import Data.Type.Equality
 import GHC.Generics
@@ -41,12 +56,12 @@ type family GetFieldPaths s (name :: Symbol) g :: PathTree Symbol where
 type family GetNamePath (name :: Symbol) g (acc :: [Path]) :: Either Symbol [Path] where
   GetNamePath name (M1 D _ g) acc = GetNamePath name g acc
 
-  -- Find path to a constructor in a sum type
+  -- Find path to a constructor in a sum type.
   GetNamePath name (M1 C ('MetaCons name _ _) _) acc = 'Right (Reverse acc '[])
   GetNamePath name (g1 :+: g2) acc = FirstRight (GetNamePath name g1 ('PathLeft  : acc))
                                                 (GetNamePath name g2 ('PathRight : acc))
 
-  -- Find path to a field in a product type
+  -- Find path to a field in a product type.
   GetNamePath name (M1 S ('MetaSel ('Just name) _ _ _) _) acc = 'Right (Reverse acc '[])
   GetNamePath name (g1 :*: g2) acc = FirstRight (GetNamePath name g1 ('PathLeft  : acc))
                                                 (GetNamePath name g2 ('PathRight : acc))
@@ -57,44 +72,53 @@ type family GetNamePath (name :: Symbol) g (acc :: [Path]) :: Either Symbol [Pat
 -- Paths to a position
 
 -- | Compute paths to a field at a specific position.
-type family GetPositionPaths s (n :: Nat) g :: PathTree (Nat, Nat) where
-  GetPositionPaths s n (M1 D _ g)  = GetPositionPaths s n g
-  GetPositionPaths s n (g1 :+: g2) = 'PathTree (GetPositionPaths s n g1)
-                                               (GetPositionPaths s n g2)
-  GetPositionPaths s n (M1 C _ g)  = 'PathLeaf (GetPositionPath n g 1 '[])
+type family GetPositionPaths s (pos :: Nat) g :: PathTree (Nat, Nat) where
+  GetPositionPaths s pos (M1 D _ g)  = GetPositionPaths s pos g
+  GetPositionPaths s pos (g1 :+: g2) = 'PathTree (GetPositionPaths s pos g1)
+                                                 (GetPositionPaths s pos g2)
+  GetPositionPaths s pos (M1 C _ g)  = 'PathLeaf (GetPositionPath pos g 1 '[])
 
-  GetPositionPaths s n V1 = TypeError ('Text "Type " ':<>: QuoteType s ':<>:
-                                       'Text " has no data constructors")
+  GetPositionPaths s pos V1 = TypeError ('Text "Type " ':<>: QuoteType s ':<>:
+                                         'Text " has no data constructors")
 
 -- | Compute path to a constructor in a sum or a field in a product at a
 -- specific position.
-type family GetPositionPath (n :: Nat) g (k :: Nat) (acc :: [Path])
+type family GetPositionPath (pos :: Nat) g (k :: Nat) (acc :: [Path])
   :: Either (Nat, Nat) [Path] where
-  GetPositionPath n (M1 D _ g) k acc = GetPositionPath n g k acc
+  GetPositionPath pos (M1 D _ g) k acc = GetPositionPath pos g k acc
 
-  -- Find field at a position in a sum type
-  GetPositionPath n (M1 C _ _) k acc =
-    If (n == k) ('Right (Reverse acc '[])) ('Left '(n, k))
-  GetPositionPath n (g1 :+: g2) k acc =
-    ContinueWhenLeft (GetPositionPath n g1 k ('PathLeft : acc)) g2 acc
+  -- Find field at a position in a sum type.
+  GetPositionPath pos (M1 C _ _) k acc =
+    If (pos == k) ('Right (Reverse acc '[])) ('Left '(pos, k))
+  GetPositionPath pos (g1 :+: g2) k acc =
+    ContinueWhenLeft (GetPositionPath pos g1 k ('PathLeft : acc)) g2 acc
 
-  -- Find field at a position in a product type
-  GetPositionPath n (M1 S _ _) k acc =
-    If (n == k) ('Right (Reverse acc '[])) ('Left '(n, k))
-  GetPositionPath n (g1 :*: g2) k acc =
-    ContinueWhenLeft (GetPositionPath n g1 k ('PathLeft : acc)) g2 acc
+  -- Find field at a position in a product type.
+  GetPositionPath pos (M1 S _ _) k acc =
+    If (pos == k) ('Right (Reverse acc '[])) ('Left '(pos, k))
+  GetPositionPath pos (g1 :*: g2) k acc =
+    ContinueWhenLeft (GetPositionPath pos g1 k ('PathLeft : acc)) g2 acc
 
-  GetPositionPath n _ k _ = 'Left '(n, k)
+  -- The second element is the number of fields in the data constructor.
+  GetPositionPath pos _ k _ = 'Left '(pos, k)
 
 -- | If the left branch had the position we're looking for, return it. Otherwise
 -- continue with the right branch.
 type family ContinueWhenLeft (r :: Either (Nat, Nat) [Path]) g acc
   :: Either (Nat, Nat) [Path] where
-  ContinueWhenLeft ('Right path)   _ _   = 'Right path
-  ContinueWhenLeft ('Left '(n, k)) g acc = GetPositionPath n g (k + 1) ('PathRight : acc)
+  ContinueWhenLeft ('Right path) _ _ = 'Right path
+  ContinueWhenLeft ('Left '(pos, k)) g acc =
+    GetPositionPath pos g (k + 1) ('PathRight : acc)
 
 ----------------------------------------
 -- Misc
+
+data Void1 a
+-- | Generate bogus equality constraints that attempt to unify generic
+-- representations with this type in case there is an error such as missing
+-- field, constructor etc. so these huge types don't leak into error messages.
+type family HideReps (g :: Type -> Type) (h :: Type -> Type) :: Constraint where
+  HideReps g h = (g ~ Void1, h ~ Void1)
 
 -- | Check if any leaf in the tree has a '[Path]'.
 type family AnyHasPath (path :: PathTree e) :: Bool where
@@ -104,5 +128,5 @@ type family AnyHasPath (path :: PathTree e) :: Bool where
 
 type family NoGenericError t where
   NoGenericError t = TypeError
-     ('Text "Type " ':<>: QuoteType t ':<>:
-      'Text " doesn't have a Generic instance")
+    ('Text "Type " ':<>: QuoteType t ':<>:
+     'Text " doesn't have a Generic instance")
