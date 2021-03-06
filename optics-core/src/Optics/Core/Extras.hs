c9b632cfc71a5e338203a69db67b15773c796661
Tom Ellis <tom-git@jaguarpaw.co.uk>
2021-03-06 11:30:08 +0000

    Add is
    (the opposite of isn't)

---
+++ optics-core/src/Optics/Core/Extras.hs
@@ -0,0 +1,22 @@
+module Optics.Core.Extras
+  (
+    is
+  )
+  where
+
+import Data.Maybe
+
+import Optics.Optic
+import Optics.AffineFold
+
+-- | Check to see if this 'AffineFold' matches.
+--
+-- >>> is _Just Nothing
+-- False
+--
+is :: Is k An_AffineFold => Optic' k is s a -> s -> Bool
+is k s = isJust (preview k s)
+{-# INLINE is #-}
+
+-- $setup
+-- >>> import Optics.Core
