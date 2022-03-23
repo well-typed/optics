a130506f0edd4a8f90caf985445864f60ec6cd6a
David Feuer <David.Feuer@gmail.com>
2022-03-23 11:21:52 -0400

    Better inference for AppendIndices (#451)
    * Better inference for AppendIndices
    
    * Allow the second argument of `AppendIndices` to be determined by the first and third arguments.
    * Refine the documentation for `AppendIndices`. Due to the way incoherent instances work, the "short-circuit" path is actually treated as a fall-back; it will *only* be taken if nothing is known about the shape of the first list.
    
    * Update optics-core/src/Optics/Internal/Optic/TypeLevel.hs
    
    Co-authored-by: Andrzej Rybczak <arybczak@users.noreply.github.com>
    
    Co-authored-by: Andrzej Rybczak <arybczak@users.noreply.github.com>

--- optics-core/src/Optics/Internal/Optic/TypeLevel.hs
+++ optics-core/src/Optics/Internal/Optic/TypeLevel.hs
@@ -111,12 +111,12 @@ data IxEq i is js where
 class AppendIndices xs ys ks | xs ys -> ks where
   appendIndices :: IxEq i (Curry xs (Curry ys i)) (Curry ks i)
 
--- | If the second list is empty, we can shortcircuit and pick the first list
--- immediately.
-instance {-# INCOHERENT #-} AppendIndices xs '[] xs where
+-- | If the second list is empty, we can pick the first list
+-- even if nothing is known about it.
+instance {-# INCOHERENT #-} xs ~ zs => AppendIndices xs '[] zs where
   appendIndices = IxEq
 
-instance AppendIndices '[] ys ys where
+instance ys ~ zs => AppendIndices '[] ys zs where
   appendIndices = IxEq
 
 instance AppendIndices xs ys ks => AppendIndices (x ': xs) ys (x ': ks) where
